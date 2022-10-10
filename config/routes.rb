3f0ae1dad7b233d0414489c355aa2e577f285c7f
myelin0 <emi.veisllari@gmail.com>
2022-10-10 18:32:22 +0200

    Add devise and create routes for app

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get 'users/sign_out' => 'devise/sessions#destroy'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :foods, except: :update
+  resources :recipes, except: :update
+  resources :public_recipes 
+  resources :general_shopping_list 
   # Defines the root path route ("/")
-  root "home#index'
+  root "recipes#index"
 end
